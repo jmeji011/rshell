7aa6d3d6f11fa751c4271c2ebd685d27f28f5901
Juan Mejia <jmeji011@hammer.cs.ucr.edu>
2015-04-19 19:46:24 -0700

    added parse function

--- src/main.cpp
+++ src/main.cpp
@@ -32,6 +32,7 @@ void prompt()
 	cout << "$ ";
 }
 
+//this function exits the program if "exit" is typed.
 bool exitshell(char** close)
 {
 	string abort = *close;
@@ -42,6 +43,43 @@ bool exitshell(char** close)
 		return true;
 	}
 	else return false;
+}
+
+//this function parses the command string
+void parse(string& str, char** dilem, char*& token)
+{
+	char** dilem1 = (char**) malloc (BUFSIZ);
+	bool tokenbool = false;
+	int cntr = 0;
+	string space = " ",
+			andkey = "&&",
+			orkey = "||",
+			semi = ";";
+	string input;
+	size_t sz1 = -1;
+	
+	while(token != NULL && !tokenbool)
+	{
+		string t = token;
+		
+		if((t.find(andkey) != sz1) || (t.find(orkey) != sz1) || (t.find(semi) != sz1)) tokenbool = true;
+		
+		if(!tokenbool) dilem1[cntr] = token;
+
+		else
+		{
+			dilem1[cntr] = NULL;
+			str = t;		
+		}
+		
+		cntr++;
+		token = strtok(NULL, " "); 
+	}
+
+	if(!tokenbool) dilem1[cntr] = NULL;
+	
+	dilem = dilem1;
+	
 }
 
 int main(int argc, char* argv[])
