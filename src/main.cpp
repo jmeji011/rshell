f423372dbfc6b7b66511bd6d0fc3b29ea3801a23
Juan Mejia <jmeji011@hammer.cs.ucr.edu>
2015-04-19 19:07:57 -0700

    added prompt function

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <stdio.h>
+#include <cstring>
+#include <vector>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 using namespace std;
 
-int main()
+void prompt()
 {
+	char* log = getlogin();
+	
+	if(!getlogin())
+	{
+		perror("getlogin()");
+	}
 
+	char host[BUFSIZ];
+	
+	if(gethostname(host, BUFSIZ) == -1)
+	{
+		perror("gethostname()");
+	}
+
+	cout << log;
+	cout << "@" ;
+	cout << host;
+	cout << ":";
+	cout << "$ ";
+}
+
+int main(int argc, char* argv[])
+{
+	prompt();
 
 
 	return 0;
