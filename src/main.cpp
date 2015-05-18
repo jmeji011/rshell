ae89f3b1662ec5ae31b5f0b84ae3262dc080c65e
Juan Mejia <jmeji011@hammer.cs.ucr.edu>
2015-05-18 03:41:28 -0700

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -83,6 +83,7 @@ void readi(char strg[])
     if(strg[length-1] == '\n') strg[length-1] = '\0';
 }
 
+//these four functions depend on how many '<' or '>' there are
 void lengtha(char cntrl[], int &length, int &a)
 {
 	length = length + 2;
@@ -171,9 +172,564 @@ void lengthd(char cntrl[], int &length, int &a)
 	a = a + 5;
 }
 
+void redctrl(const vector<struct redirect*> &file)
+{
+	if(file.size() == 0) return;
+    
+	for(unsigned i = 0; i < file.size(); ++i)
+	{
+		int filedes;
+        
+		int filedes1[2];
+        
+		int lengtha = strlen(file.at(i)->file);
+        
+		
+		char temp[500];
+        
+		switch(file.at(i)->io)
+		{
+			case 0: 
+				filedes = open(file.at(i)->file, O_CREAT|O_RDWR|O_TRUNC, S_IRUSR|S_IWUSR);
+                
+				if(filedes == -1)
+				{
+					perror("open()0");
+					exit(1);
+				}
+                
+				if(dup2(filedes, file.at(i)->filedes) == -1)
+				{
+					perror("dup2()0");
+					exit(1);
+				}
+				break;
+                
+			case 1: 
+            
+				filedes = open(file.at(i)->file, O_CREAT|O_RDWR|O_APPEND, S_IRUSR|S_IWUSR);
+                
+				if(filedes == -1)
+				{
+					perror("open()1");
+					exit(1);
+				}
+                
+				if(dup2(filedes, file.at(i)->filedes) == -1)
+				{
+					perror("dup2()1");
+					exit(1);
+				}
+				break;
+                
+			case 2: 
+            
+				filedes = open(file.at(i)->file, O_RDONLY);
+                
+				if(filedes == -1)
+				{
+					perror("open()2");
+					exit(1);
+				}
+                
+				if(dup2(filedes, file.at(i)->filedes) == -1)
+				{
+					perror("dup2()2");
+				}
+                
+				break;
+                
+			case 3: 
+				strcpy(temp, file.at(i)->file);
+                
+				strcat(temp, "\n");
+                
+				if(-1 == pipe(filedes1))
+				{
+					perror("pipe()");
+					exit(1);
+				}
+                
+				if(write(filedes1[wpipe], temp, lengtha + 1) == -1)
+				{
+					perror("write()");
+					exit(1);
+				}
+                
+				if(dup2(filedes1[rpipe], 0) == -1)
+				{
+					perror("dup2()");
+					exit(1);
+				}
+                
+				if(close(filedes1[wpipe]) == -1)
+				{
+					perror("close()");
+					exit(1);
+				}
+				break;
+		}
+	}
+}
 
+
+//determines how many '<' or '>' there are
+void org(char cntrl[], int &f)
+{
+    char ink = '<',
+           outk = '>';
+	f = 0;
+    
+	string temp;
+    
+	int length = strlen(cntrl);
+    
+	for (int i = 0; i < length; ++i)
+	{
+		if (cntrl[i] == '#')
+		{
+			cntrl[i] = '\0';
+            
+			break;
+		}
+	}
+    
+	length = strlen(cntrl);
+    
+	for (int i = 0; i < length; ++i)
+	{
+		if (cntrl[i] == '\'' || cntrl[i] == '\"')
+		{
+			for (int j = i; j < length; ++j)
+			{
+				cntrl[j] = cntrl[j+1];
+			}
+			--length;
+			--i;
+		}
+	}
+    
+	length = strlen(cntrl);
+    
+	for (int i = 0; i < length; ++i)
+	{
+		if (cntrl[i] == '\t')
+		{
+			cntrl[i] = ' ';
+		}
+	}
+	
+	for (int i = 0; i < length; ++i)
+	{
+		if (i == 0)
+		{
+			if (isdigit(cntrl[i]))
+			{
+				if (cntrl[i+1] == ink && cntrl[i+2] != ink)
+                {
+					lengthb(cntrl, length, i);
+                }
+				else if (cntrl[i+1] == ink && cntrl[i+2] == ink && cntrl[i+3] == ink)
+                {
+					lengthd(cntrl ,length, i);
+                }
+				else if (cntrl[i+1] == outk && cntrl[i+2] != outk)
+                {
+					lengthb(cntrl, length, i);
+                }
+				else if (cntrl[i+1] == outk && cntrl[i+2] == outk)
+                {
+					lengthc(cntrl, length, i);
+                }
+			}
+			else if (cntrl[i] == ink && cntrl[i+1] != ink)
+            {
+				lengtha(cntrl, length, i);
+            }
+			else if (cntrl[i] == ink && cntrl[i+1] == ink && cntrl[i+2] == ink)
+            {
+				lengthc(cntrl, length, i);
+            }
+			else if (cntrl[i] == outk && cntrl[i+1] != outk)
+            {
+				lengtha(cntrl, length, i);
+            }
+			else if (cntrl[i] == outk && cntrl[i+1] == outk)
+            {
+				lengthb(cntrl, length, i);
+            }
+			else if (cntrl[i] == '|')
+            {
+				lengtha(cntrl, length, i);
+            }
+
+		}
+		else
+		{
+			if (isdigit(cntrl[i]) && cntrl[i-1] == ' ')
+			{
+				if (cntrl[i+1] == ink && cntrl[i+2] != ink)
+                {
+					lengthb(cntrl, length, i);
+                }
+				else if (cntrl[i+1] == ink && cntrl[i+2] == ink && cntrl[i+3] == ink)
+                {
+					lengthd(cntrl, length, i);
+                }
+				else if (cntrl[i+1] == outk && cntrl[i+2] != outk)
+                {
+					lengthb(cntrl, length, i);
+                }
+				else if (cntrl[i+1] == outk && cntrl[i+2] == outk)
+                {
+					lengthc(cntrl, length, i);
+                }
+			}
+			else if (cntrl[i] == ink && cntrl[i+1] != ink)
+            {
+				lengtha(cntrl, length, i);
+            }
+			else if (cntrl[i] == ink && cntrl[i+1] == ink && cntrl[i+2] == ink)
+            {
+				lengthc(cntrl, length, i);
+            }
+			else if (cntrl[i] == outk && cntrl[i+1] != outk)
+            {
+				lengtha(cntrl, length, i);
+            }
+			else if (cntrl[i] == outk && cntrl[i+1] == outk)
+            {
+				lengthb(cntrl, length, i);
+            }
+			else if (cntrl[i] == '|')
+            {
+				lengtha(cntrl, length, i);
+            }
+	
+		}
+	}
+	
+	
+	for (int i = 0; i < length; ++i)
+	{
+		if (cntrl[i] == ' ')
+		{
+			if (cntrl[i+1] == ' ')
+			{
+				for (int j = i; j < length; ++j)
+				{
+					cntrl[j] = cntrl[j+1];
+				}
+				--i;
+				--length;
+			}
+		}
+	}
+
+	length = strlen(cntrl);
+    
+	while (cntrl[0] == ' ')
+	{
+		for (int i = 0; i < length; ++i)
+		{
+			cntrl[i] = cntrl[i+1];
+		}
+	}
+
+	length = strlen(cntrl);
+    
+	while (cntrl[length-1] == ' ')
+	{
+		cntrl[length-1] = '\0';
+		-- length;
+	}
+
+	for (int i = 0; cntrl[i+2] != '\0'; ++i)
+	{
+		if(cntrl[i] == '|' && cntrl[i+2] == '|')
+		{
+			f = 1;
+		}
+	}
+}
+
+bool numctrl(string s, int &io)
+{
+	if (isdigit(s.at(0)))
+	{
+		s.erase(s.begin());
+        
+		if(s == ">")
+        {
+			io = 0;
+			return true;
+		}
+		
+        else if(s == "<<<")
+        {
+			io = 3;
+			return true;
+		}
+		else if(s == "<")
+        {
+			io = 2;
+			return true;
+		}
+        else if(s == ">>")
+        {
+			io = 1;
+			return true;
+		}
+		
+	}
+	return false;
+}
+
+void parse (char *comm[], char cntrl[], vector<struct redirect*> &file, int &f)
+{
+	f = 0;
+    char * get[500];
+	struct redirect* temp = 0;
+	vector<string> s;
+	char * tk, *ptr;
+	string tempa;
+    
+	tk = strtok_r(cntrl, " ", &ptr);
+    
+	int c = 0;
+    
+	get[c] = tk;
+    
+	tempa = tk;
+
+	while (tk != NULL)
+	{
+		++c;
+		tempa = tk; 
+        
+		s.push_back(tempa);
+        
+		tk = strtok_r(NULL, " ", &ptr);
+        
+		get[c] = tk;
+	}
+
+	int j = 0;
+	int filedes;
+	int io;
+    
+	for (unsigned i = 0; i < s.size()-1; ++i)
+	{
+		if((s.at(i+1)== "<" || s.at(i+1) == "<<<" || s.at(i+1) == ">" || s.at(i+1) == ">>" || numctrl(s.at(i+1), io)) && (s.at(i)== "<" || s.at(i)=="<<<" || s.at(i)== ">" ||s.at(i) == ">>" || numctrl(s.at(i), io))) f= 1;
+	}
+
+	for(unsigned i = 0; i < s.size(); ++i)
+	{	
+			if(s.at(i) == ">") 
+			{
+				temp = new struct redirect(get[i+1], 0, 1);
+                
+				file.push_back(temp);
+                
+				++i;
+			}
+            
+            else if(s.at(i) == "<") 
+			{
+			
+				temp = new struct redirect(get[i+1], 2, 0);
+				file.push_back(temp);
+				++i;
+			}
+            
+			else if(s.at(i) == ">>") 
+			{
+				
+				temp = new struct redirect(get[i+1], 1, 1);
+                
+				file.push_back(temp);
+                
+				++i;
+			}
+			
+            else if(numctrl(s.at(i), io))
+			{
+				filedes = s.at(i)[0] - '0';
+                
+				temp = new struct redirect(get[i+1], io, filedes);
+                
+				file.push_back(temp);
+				++i;
+			}
+            
+			else if(s.at(i) == "<<<") 
+			{
+
+				temp = new struct redirect(get[i+1], 3, 0);
+                
+				file.push_back(temp);
+				++i;
+			}
+        
+			else
+			{
+				comm[j] = get[i];
+				++j;
+			}
+
+	}
+}
+
+void exec(char cntrl[], int save_stdin = -1)
+{
+	
+	char *left;
+    char *right;
+    char *comm[500] = {0};
+    int f = 0;
+	unsigned i = 0;
+	
+	for (i = 0; i < strlen(cntrl); ++i)
+	{
+		if (cntrl[i] == '|' && i != 0 && i != strlen(cntrl)-1)
+		{
+			left = cntrl; 
+            
+			cntrl[i] = '\0';
+             
+			right = cntrl + i + 1;
+
+			int filedes[2];
+            
+			if(-1 == pipe(filedes))
+			{
+				perror("pipe()");
+				exit(1);
+			}
+            
+			pid_t pid;
+            
+			if(-1 == (pid = fork()))
+			{
+				perror("fork()");
+				exit(1);
+			}
+            
+			else if(pid == 0) 
+			{
+				if(-1 == dup2(filedes[wpipe], 1))
+				{
+					perror("dup2()");
+					exit(1);
+				}	
+                
+				if(-1 == close(filedes[rpipe]))
+				{
+					perror("close()");
+					exit(1);
+				}
+                
+				exec(left, save_stdin);
+                
+				exit(0);
+			}
+            
+			else 
+			{
+				
+				if (save_stdin == -1)
+				{
+					if(-1 == (save_stdin = dup(0)))
+					{
+						perror("dup()");
+						exit(1);
+					}
+				}	
+				
+				if(close(filedes[wpipe]) == -1)
+				{
+					perror("close()");
+					exit(1);
+				}
+				
+				if(dup2(filedes[rpipe], 0) == -1)
+				{
+					perror("dup2()");
+					exit(1);
+				}
+				exec(right, save_stdin);
+                
+				if(dup2(save_stdin, 0) == -1)
+				{
+					perror("dup2()");
+					exit(1);
+				}
+				
+				if(close(filedes[rpipe]) == -1)
+				{
+					perror("close()");
+					exit(1);
+				}
+				
+				if(wait(0) == -1)
+				{
+					perror("wait()");
+					exit(1);
+				}
+				return;
+			}
+		}
+	}
+	vector<struct redirect*> files;
+    
+	parse(comm, cntrl, files, f);
+	if(f == 1)
+	{
+		cerr << "Syntax error." << endl;
+		return;
+	}
+	pid_t pid;
+	int status;
+	if ((pid = fork()) < 0)
+	{
+		perror("fork failed\n");
+		r = false;
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		redctrl(files);
+		
+		if (execvp(*comm, comm) < 0)
+		{
+			perror ("exec failed\n");
+			r = false;
+			exit(1);
+		}
+	}
+	else
+	{
+		if (-1 == waitpid(pid, &status, 0))
+		{
+			perror("wait error\n");
+			r = false;
+			exit(1);
+		}
+		if (WEXITSTATUS(status) == 0)
+		{
+			r = true;
+		}
+		else if (WEXITSTATUS(status) == 1)
+		{
+			r = false;
+		}
+	}
+	return;
+}
+
+/*
 //this function parses the command string
-void parse(string& str, char**& dilem, char*& token)
+void parse2(string& str, char**& dilem, char*& token)
 {
 	char** dilem1 = (char**) malloc (BUFSIZ);
 	bool tokenbool = false;
@@ -208,6 +764,8 @@ void parse(string& str, char**& dilem, char*& token)
 	dilem = dilem1;
 	
 }
+
+
 
 bool execute(char** arg)	
 {
@@ -249,11 +807,16 @@ bool execute(char** arg)
 	}
 	return true;
 }
-int main(int argc, char* argv[])
+*/
+int main(int argc, char**argv)
 {
-	while(cin.good())
+    vector<struct redirect*> file;
+	int f = 0;
+	while(1)
 	{
-		string input;
+        prompt();
+        /*
+        		string input;
 		string space = " ",
 				andkey = "&&",
 				orkey = "||",
@@ -261,8 +824,6 @@ int main(int argc, char* argv[])
 				hashtag = "#";
 		
 		//display prompt
-		prompt();
-
 		getline(cin, input);
 		
 		int num1 = input.find(hashtag,0);
@@ -324,7 +885,7 @@ int main(int argc, char* argv[])
 			
 			while(tkn != NULL)
 			{
-				parse(str, dilem, tkn);
+				parse2(str, dilem, tkn);
 
 				if(*dilem != NULL && booland && !boolor)
 				{
@@ -391,9 +952,52 @@ int main(int argc, char* argv[])
 				delete dilem;
 			}
 			break;
+		}*/
+        char ctrl[500];
+        
+		memset(ctrl, '\0', 500);
+
+		readi(ctrl);
+		org(ctrl, f);
+		
+		if(f == 1)
+		{
+			cerr << "Syntax Error." << endl;
+			continue;
 		}
 
+		if (ctrl[strlen(ctrl)-1] == '>' || ctrl[strlen(ctrl)-1] == '<')
+		{
+			cerr << "Invalid number of arguments. " << endl;
+			continue;
+		}
+        
+		if (ctrl[0] == '\0') continue;
+            
+		if (strncmp(ctrl, "exit", 4) == 0)
+		{
+			if (ctrl[4] == '\0' || ctrl[4] == ' ')
+			{
+				cout << "End of program." << endl;
+				exit(0);
+			}
+		}
+		exec(ctrl);
+        
+		int length = file.size();
+        
+		for (int i = 0; i < length; ++i)
+		{
+			if (file.at(i) != 0) 
+            {
+				delete file.at(i);
+				file.at(i) = 0;
+			}
+		}
+        
+		file.clear();
 	}
-
-	return 0;
+    
+    return 0;
 }
+
